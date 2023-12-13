6cc5dc3f05f9e127bc4dfb8fc41f707b12ec6dae
Bash-Abdul <bashirtaiwo59@gmail.com>
2023-12-14 00:28:31 +0100

    new sections

--- js/script.js
+++ js/script.js
@@ -21,3 +21,27 @@ var swiperCategories     = new Swiper(".categories__container", {
     },
   },
 });
+
+
+
+// PRODUCTS TAB
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach((tab) =>{
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) =>{
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) =>{
+      tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+  });
+
+});
